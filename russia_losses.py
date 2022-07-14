7f3c027b88f9f563d1cd622c7e147f64a7839f19
Petro <pivan@softserveinc.com>
2022-07-15 01:18:08 +0300

    add weekly losses plot

--- russia_losses.py
+++ russia_losses.py
@@ -1,49 +1,142 @@
 import streamlit as st
 import json
 import requests
+from dateutil.relativedelta import relativedelta
+
 import pandas as pd
+import numpy as np
+import plotly.figure_factory as ff
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-from datetime import date
-from dateutil.relativedelta import relativedelta
+
+
+def preprocess_dataframe_equipment(url):
+    columns_to_sum = ['military auto', 'fuel tank', 'vehicles and fuel tanks']
+    columns_to_drop = columns_to_sum + ['mobile SRBM system', 'greatest losses direction']
+
+    df = pd.DataFrame(requests.get(url).json())
+    df['date'] = pd.to_datetime(df['date'])
+    df['day'] = df['day'].astype(int)
+    df['vehicle and fuel tank'] = df[columns_to_sum].sum(axis=1).astype(int)
+    df = df.drop(columns_to_drop, axis=1)
+    return df
+
+def create_dataframe_heatmap(df, columns_list):
+    '''
+    Converts input dataset into dataset ready to use for heatmap chart
+    '''
+    df_week = df.copy()
+    df_week['week_event'] = df_week['date'].dt.week
+    df_week['week_label'] = df_week['date'].dt.to_period('W-SUN').apply(lambda r: r.end_time).dt.strftime('%b')+' '+\
+                            df_week['date'].dt.to_period('W-SUN').apply(lambda r: r.end_time).dt.strftime('%d')
+
+    list_df = []
+    for column in columns_list:
+        df_temp = df_week[[column, 'week_event']].copy()
+        df_temp['name'] = column
+        df_temp_pivot = pd.pivot_table(df_temp, values=column, index='name', columns='week_event', aggfunc=np.sum)
+        list_df.append(df_temp_pivot)
+    df_output = pd.concat(list_df, axis=0)
+
+    x_labels_list = list(df_week['week_label'].unique())
+    y_labels_list = list(df_output.index)
+
+    return df_output, x_labels_list, y_labels_list
+
+def plot_heat_map(values_input, values_heat_map, x_label, y_label):
+    '''
+    Plot heatmap chart
+    '''
+    fig = ff.create_annotated_heatmap(
+        values_input[::-1],
+        x=x_label,
+        y=y_label[::-1], 
+        annotation_text=values_heat_map[::-1], 
+        text=values_heat_map[::-1],
+        colorscale='Greys', 
+        hoverinfo='text'
+        )
+    fig.update_layout(
+        title_text='<b>Weekly losses</b>',
+        title_x=0.5,
+        )
+    st.plotly_chart(fig, use_container_width=True)
+
+def plot_bar_line_plot(df, df_daily, columns_list, index_selected, date_last):
+    '''
+    Plot bar and line charts together
+    '''
+    fig = make_subplots(2, 1, subplot_titles=("<b>Daily Losses</b>", "<b>Total Losses</b>"))
+    fig.add_trace(
+        go.Bar(
+            x=df_daily['date'],
+            y=df_daily[columns_list[index_selected]],
+            text=df_daily[columns_list[index_selected]]
+            ), 
+        row=1, 
+        col=1)
+    fig.add_trace(
+        go.Scatter(x=df['date'], 
+        y=df[columns_list[index_selected]], 
+        mode='lines+markers'), 
+        row=2, 
+        col=1
+        )
+    fig.update_layout(
+        xaxis=dict(
+            range=[date_last - relativedelta(months=+2), date_last],
+            rangeselector=dict(
+                buttons=list([
+                    dict(count=1, label="last month", step="month", stepmode="backward"),
+                    dict(count=2, label="last 2 months", step="month", stepmode="backward"),
+                    dict(count=3, label="last 3 months", step="month", stepmode="backward"),
+                    dict(label="all time", step="all")
+                    ])
+            ),
+            type="date",
+        ),
+        xaxis2=dict(
+            rangeslider=dict(visible=True, thickness=0.05),
+            type="date",
+        ),
+    )
+    fig.update_layout(
+        height=800,
+        showlegend=False,
+    )
+    fig.update_xaxes(matches='x')
+    st.plotly_chart(fig, use_container_width=True)
+
 
 period_last = 7
 
 url1 = 'https://raw.githubusercontent.com/PetroIvaniuk/2022-Ukraine-Russia-War-Dataset/main/data/russia_losses_equipment.json'
 url2 = 'https://raw.githubusercontent.com/PetroIvaniuk/2022-Ukraine-Russia-War-Dataset/main/data/russia_losses_personnel.json'
 
-response1 = requests.get(url1)
-response2 = requests.get(url2)
-
-df_personnel = pd.DataFrame(response2.json())
+df_personnel = pd.DataFrame(requests.get(url2).json())
 df_personnel_daily = df_personnel[['date', 'personnel']].copy().set_index('date')
 df_personnel_daily = df_personnel_daily.diff().fillna(df_personnel_daily).fillna(0).astype(int).reset_index()
 
 total_losses_personnel = df_personnel.iloc[-1]['personnel']
 total_losses_personnel_period = df_personnel_daily.tail(period_last).sum()['personnel']
 
-columns_sum = ['military auto', 'fuel tank', 'vehicles and fuel tanks']
-columns_drop = columns_sum + ['mobile SRBM system', 'greatest losses direction']
+df_equipment = preprocess_dataframe_equipment(url1)
+df_equipment_daily = df_equipment.copy().set_index(['date', 'day'])
+df_equipment_daily = df_equipment_daily.diff().fillna(df_equipment_daily).fillna(0).astype(int).reset_index()
 
-df = pd.DataFrame(response1.json())
-df['date'] = pd.to_datetime(df['date'])
-df['vehicle and fuel tank'] = df[columns_sum].sum(axis=1).astype(int)
-df = df.drop(columns_drop, axis=1)
-
-df_daily = df.copy().set_index(['date', 'day'])
-df_daily = df_daily.diff().fillna(df_daily).fillna(0).astype(int).reset_index()
-
-df_sum = df_daily.loc[:, df_daily.columns!='day'].sum()
-df_sum_period = df_daily.loc[:, df_daily.columns!='day'].tail(period_last).sum()
+df_sum = df_equipment_daily.loc[:, df_equipment_daily.columns!='day'].sum()
+df_sum_period = df_equipment_daily.loc[:, df_equipment_daily.columns!='day'].tail(period_last).sum()
 total_losses = df_sum.sum()
 total_losses_peroid = df_sum_period.sum()
 
-date_last = df_daily.iloc[-1]['date'].date()
-day_last = df_daily.iloc[-1]['day']
-columns_data = df_daily.loc[:, ~df_daily.columns.isin(['date', 'day'])].columns
+date_last = df_equipment_daily.iloc[-1]['date'].date()
+day_last = df_equipment_daily.iloc[-1]['day']
+columns_equipment_list = df_equipment_daily.loc[:, ~df_equipment_daily.columns.isin(['date', 'day'])].columns
+
 
 st.set_page_config(page_title='War-Losses', page_icon="🇺🇦", layout="wide")
 
+# metrics part
 with st.container():
     _, col001, _ = st.columns((1.8, 1.9, 1.8))
     with col001:
@@ -63,14 +156,14 @@ with st.container():
     _, col101, col102, col103, col104, col105, col106, _ = st.columns((1.25, 1, 1, 1, 1, 1, 1, 1.25))
     _, col107, col108, col109, col110, col111, col112, _ = st.columns((1.25, 1, 1, 1, 1, 1, 1, 1.25))
 
-    columns_lsit = [col101, col102, col103, col104, col105, col106, 
-                    col107, col108, col109, col110, col111, col112,]
+    columns_metric = [col101, col102, col103, col104, col105, col106, 
+                      col107, col108, col109, col110, col111, col112,]
 
-    for i, col in enumerate(columns_lsit):
+    for i, col in enumerate(columns_metric):
         col.metric(
-            columns_data[i],
-            int(df_sum[columns_data[i]]),
-            int(df_sum_period[columns_data[i]]
+            columns_equipment_list[i],
+            int(df_sum[columns_equipment_list[i]]),
+            int(df_sum_period[columns_equipment_list[i]]
                 ))
     
     _, col0022, _ = st.columns((4, 1, 4))
@@ -81,6 +174,7 @@ with st.container():
     with col0023:
         st.markdown(' Data updated on {}'.format(date_last))
 
+# plots part
 with st.container():
     _, col003, _ = st.columns((1.5, 1, 1.5))
     with col003:
@@ -88,51 +182,25 @@ with st.container():
 
     _, col004, _ = st.columns((1, 2, 1))
     with col004:
-        selected_equipment = st.selectbox(
-            label='Select an Equipment:', 
-            options=columns_data,
+        index_selected_equipment = st.selectbox(
+            label="Select an Equipment:", 
+            options=range(len(columns_equipment_list)), 
+            format_func=lambda x: columns_equipment_list[x],
             index=6
-        )
+            )
 
-    # _, col005, _ = st.columns((0.25, 2, 0.25))
-    # with col005:
-    fig = make_subplots(2, 1, subplot_titles=("Daily Losses", "Total Losses"))
-    fig.add_trace(go.Bar(
-        x=df_daily['date'],
-        y=df_daily[selected_equipment],
-        text=df_daily[selected_equipment]
-        ), row=1, col=1)
-    fig.add_trace(go.Scatter(x=df['date'], y=df[selected_equipment], mode='lines+markers'), row=2, col=1)
+    # plot bar and line chharts
+    plot_bar_line_plot(df_equipment, df_equipment_daily, columns_equipment_list, index_selected_equipment, date_last)
 
-    fig.update_layout(
-        xaxis=dict(
-            range=[date_last - relativedelta(months=+2), date_last],
-            rangeselector=dict(
-                buttons=list([
-                    dict(count=1, label="last month", step="month", stepmode="backward"),
-                    dict(count=2, label="last 2 months", step="month", stepmode="backward"),
-                    dict(count=3, label="last 3 months", step="month", stepmode="backward"),
-                    dict(label="all time", step="all")
-                    ])
-            ),
-            type="date",
-        ),
-        xaxis2=dict(
-            rangeslider=dict(visible=True, thickness=0.05),
-            type="date",
-        ),
-    )
+    # plot heatmap
+    df_heat_map, x_labels_list, y_labels_list = create_dataframe_heatmap(df_equipment_daily, columns_equipment_list)
+    values_heat_map = df_heat_map.values
+    values_input = np.zeros(values_heat_map.shape)
+    values_input[index_selected_equipment]=values_heat_map[index_selected_equipment]
+    plot_heat_map(values_input, values_heat_map, x_labels_list, y_labels_list)
 
-    fig.update_layout(
-        height=800,
-        showlegend=False,
-    )
-
-    fig.update_xaxes(matches='x')
-
-    st.plotly_chart(fig, use_container_width=True)
-
-
+# sources and about part
+with st.container():
     _, col006, _ = st.columns((2.25, 1, 2.25))
     with col006:
         st.markdown('### Sources & About')
